1ddd3a93242744fba2c02076dd0d5906f748fa81
sbogado <sbogado@secureauth.com>
2019-10-26 21:01:02 -0300

    Add redirect or something like that

--- src/main/java/ar/com/medicinas/authserver/controller/RedirectController.java
+++ src/main/java/ar/com/medicinas/authserver/controller/RedirectController.java
@@ -9,8 +9,6 @@ import org.springframework.security.oauth2.provider.ClientDetails;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
